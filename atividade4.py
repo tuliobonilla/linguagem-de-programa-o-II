e52a5e56b8570040ca21ca46ce05895cc7003510
Tulio Cicero Rohrig Bonilla <50706054+tuliobonilla@users.noreply.github.com>
2020-08-12 21:41:07 -0300

    Add files via upload

---
+++ atividade4.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+print('Digite uma valor')
+valor = int(input())
+
+print('Digite o preço')
+preco = int(input())
+
+calcular = valor - preco
+
+print("Resultado Final é:", calcular)
